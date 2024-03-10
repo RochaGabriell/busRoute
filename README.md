340c3aa0aa77d9a2be60877b2e52344161baf2ff
RochaGabriell <rochagabriell.dev@gmail.com>
2024-03-10 12:40:59 -0300

    feat[Finish Project]: Fazer alterações no cod e doc

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # busRoute
 Criar a rota do ônibus do IFPI em um mapa com Folium, adicionando cada ponto de parada e colocando uma linha de cor azul na ida (ifpi aeroporto) e de cor vermelha na volta (aeroporto ao IFPI). A rota e as paradas devem ser iguais ao que o ônibus faz hoje (ver imagem informativa)
+
+<img alt="Meme Dev.Duck" src="https://github.com/RochaGabriell/busRoute/blob/main/.github/image.png?raw=true">
--- busRoute/apps/core/views.py
+++ busRoute/apps/core/views.py
@@ -2,53 +2,51 @@ from typing import Any
 from django.views.generic import TemplateView
 from django.templatetags.static import static
 
-from django.shortcuts import render
-
 import folium
-from folium.plugins import Draw
+# from folium.plugins import Draw
 
 
 class MapView(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        coordinates = [
-            {"location": [-10.43029, -45.174006],
-                "tooltip": "TRAJETO SAÍDA DO IFPI",
-                "popup": "IFPI - Campus Corrente"},
-            {"location": [-10.438233, -45.173019],
-                "tooltip": "1ª Parada",
-                "popup": "Posto de Combustível Primavera"},
-            {"location": [-10.454374, -45.171460],
-                "tooltip": "2ª Parada",
-                "popup": "Praça Principal do Vermelhão"},
-            {"location": [-10.439672, -45.168913],
-                "tooltip": "3ª Parada",
-                "popup": "Supermercado Rocha"},
-            {"location": [-10.443239, -45.160735],
-                "tooltip": "4ª Parada",
-                "popup": "Praça da Igreja Batista"},
-            {"location": [-10.445506, -45.157068],
-                "tooltip": "5ª Parada",
-                "popup": "15ª Regional de Educação"},
-            {"location": [-10.451376, -45.146146],
-                "tooltip": "6ª Parada",
-                "popup": "Posto de Combustível do Aeroporto"},
-            {"location": [-10.454766, -45.137556],
-                "tooltip": "7ª Parada",
-                "popup": "Escola Municipal Orley Cavalcante Pacheco"},
-        ]
+        # coordinates = [
+        #     {"location": [-10.43029, -45.174006],
+        #         "tooltip": "TRAJETO SAÍDA DO IFPI",
+        #         "popup": "IFPI - Campus Corrente"},
+        #     {"location": [-10.438233, -45.173019],
+        #         "tooltip": "1ª Parada",
+        #         "popup": "Posto de Combustível Primavera"},
+        #     {"location": [-10.454374, -45.171460],
+        #         "tooltip": "2ª Parada",
+        #         "popup": "Praça Principal do Vermelhão"},
+        #     {"location": [-10.439672, -45.168913],
+        #         "tooltip": "3ª Parada",
+        #         "popup": "Supermercado Rocha"},
+        #     {"location": [-10.443239, -45.160735],
+        #         "tooltip": "4ª Parada",
+        #         "popup": "Praça da Igreja Batista"},
+        #     {"location": [-10.445506, -45.157068],
+        #         "tooltip": "5ª Parada",
+        #         "popup": "15ª Regional de Educação"},
+        #     {"location": [-10.451376, -45.146146],
+        #         "tooltip": "6ª Parada",
+        #         "popup": "Posto de Combustível do Aeroporto"},
+        #     {"location": [-10.454766, -45.137556],
+        #         "tooltip": "7ª Parada",
+        #         "popup": "Escola Municipal Orley Cavalcante Pacheco"},
+        # ]
 
         out_route = 'data/out_route.geojson'
         return_route = 'data/return_route.geojson'
         maker_route = 'data/maker_route.geojson'
 
-        m = folium.Map(location=(-10.439829, -45.16325), zoom_start=15)
+        m = folium.Map(location=(-10.439829, -45.16325), zoom_start=14)
 
-        draw = Draw(export=True)
-        draw.add_to(m)
+        # draw = Draw(export=True)
+        # draw.add_to(m)
 
-        icon_url = static('img/bus-stop-pointer.png')
+        icon_url = static('img/834px-Bus_stop_symbol.svg.ico')
 
         # for coord in coordinates:
         #     folium.Marker(
@@ -68,10 +66,12 @@ class MapView(TemplateView):
                        style_function=lambda x: {'color': 'red'}).add_to(m)
 
         folium.GeoJson(maker_route, name="Maker Router",
+                       tooltip=folium.GeoJsonTooltip(
+                           fields=['Local']),
                        marker=folium.Marker(
                            icon=folium.DivIcon(html=f"""
                               <div>
-                                 <img src="{icon_url}" style="width: 40px; height: 40px;" />
+                                 <img src="{icon_url}" style="width: 35px; height: 35px;" />
                               </div>
                               """),
                        )).add_to(m)
